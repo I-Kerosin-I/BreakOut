d20732962981133119ed0a8514341489afafdb91
I-Kerosin-I <max.andr2005@yandex.ru>
2023-08-22 12:00:26 +0300

    ellipse draw function improved mode functions get X and Y fixed functions for drawing squares fill mode added mouseup callback is now a separate function

--- editor/script.js
+++ editor/script.js
@@ -35,7 +35,7 @@ let prewX, prewY
 
 CanvasRenderingContext2D.prototype.fillBrick = function (x, y) {
     this.fillRect(x * (brickSide * 1.2), y * (brickSide * 1.2), brickSide, brickSide);
-    changeBuffer[x] ? changeBuffer[x][y] = previewCtx.fillStyle : null
+    changeBuffer[x] ? changeBuffer[x][y] = previewCtx.fillStyle : undefined
 }
 
 function clearPreview() {
@@ -68,9 +68,9 @@ function drawEllipse(X1, Y1, a, b, filled) {
         drawLine(X1 - Math.floor(a * 0.8), Y1, X1 + Math.floor(a * 0.8), Y1)
         return
     }
-    if (Y1 + b < 45) {previewCtx.fillBrick(X1, Y1 + b)} 
-    if (Y1 - b >= 0) {previewCtx.fillBrick(X1, Y1 - b)}
-    
+    if (Y1 + b < 45) { previewCtx.fillBrick(X1, Y1 + b) }
+    if (Y1 - b >= 0) { previewCtx.fillBrick(X1, Y1 - b) }
+
     while (y > 0) {
         if (b * b * x < a * a * y) {
             if (b ** 2 * (x + 1) ** 2 + a ** 2 * (y - 0.5) ** 2 - a ** 2 * b ** 2 > 0) {
@@ -83,12 +83,12 @@ function drawEllipse(X1, Y1, a, b, filled) {
             }
             y--
         }
-        if (X1 + x > 0 && Y1 - y > 0) { previewCtx.fillBrick(X1 + x, Y1 - y) }
-        if (X1 - x > 0 && Y1 - y > 0) { previewCtx.fillBrick(X1 - x, Y1 - y) }
-        if (X1 + x > 0 && Y1 + y < 45) { previewCtx.fillBrick(X1 + x, Y1 + y) }
+        if (X1 + x < 128 && Y1 - y >= 0) { previewCtx.fillBrick(X1 + x, Y1 - y) }
+        if (X1 - x > 0 && Y1 - y >= 0) { previewCtx.fillBrick(X1 - x, Y1 - y) }
+        if (X1 + x < 128 && Y1 + y < 45) { previewCtx.fillBrick(X1 + x, Y1 + y) }
         if (X1 - x > 0 && Y1 + y < 45) { previewCtx.fillBrick(X1 - x, Y1 + y) }
         if (filled) {
-            let lastX = Math.min(X1 + x, 128)
+            const lastX = Math.min(X1 + x, 128)
             for (let i = Math.max(X1 - x, 0); i < lastX; i++) {
                 if (Y1 - y >= 0) { previewCtx.fillBrick(i, Y1 - y) }
                 if (Y1 + y < 45) { previewCtx.fillBrick(i, Y1 + y) }
@@ -100,57 +100,93 @@ function drawEllipse(X1, Y1, a, b, filled) {
 
 
 const modes = [
-    function pencil(e) {
-        drawLine(prewX, prewY,
-            Math.floor((e.x) / (brickSide * 1.2)), Math.floor((e.y - 48) / (brickSide * 1.2)))
-        prewX = Math.floor(e.x / (brickSide * 1.2))
-        prewY = Math.floor((e.y - 48) / (brickSide * 1.2))
+    {
+        'callback': function pencil(X, Y) {
+            drawLine(prewX, prewY, X, Y)
+            prewX = X
+            prewY = Y
+        }, 'continuous': true
     },
-    function line(e) {
-        clearPreview()
-
-        drawLine(startX, startY, Math.floor((e.x) / (brickSide * 1.2)), Math.floor((e.y - 48) / (brickSide * 1.2)))
+    {
+        'callback': function line(X, Y) {
+            clearPreview()
+            drawLine(startX, startY, X, Y)
+        }, 'continuous': true
     },
-    function filledSquare(e) {
-        clearPreview()
-        const endBrickX = Math.floor(e.x / (brickSide * 1.2)) + 1
-        const endBrickY = Math.floor((e.y - 48) / (brickSide * 1.2)) + 1
+    {
+        'callback': function filledSquare(X, Y) {
+            clearPreview()
+            previewCtx.fillBrick(startX, startY)
 
-        for (let x = startX; x !== endBrickX; x += Math.sign(endBrickX - startX)) {
-            for (let y = startY; y !== endBrickY; y += Math.sign(endBrickY - startY)) {
-                previewCtx.fillBrick(x, y)
+            const dx = X - startX >= 0 ? 1 : -1
+            const dy = Y - startY >= 0 ? 1 : -1
+
+            for (let x = startX; x !== X + dx; x += dx) {
+                for (let y = startY; y !== Y + dy; y += dy) {
+                    previewCtx.fillBrick(x, y)
+                }
             }
-        }
+        }, 'continuous': true
     },
-    function strokeSquare(e) {
-        clearPreview()
-        const endBrickX = Math.floor(e.x / (brickSide * 1.2))
-        const endBrickY = Math.floor((e.y - 48) / (brickSide * 1.2)) + 1
+    {
+        'callback': function strokeSquare(X, Y) {
+            clearPreview()
 
-        for (let x = startX; x !== endBrickX; x += Math.sign(endBrickX - startX)) {
-            previewCtx.fillBrick(x, startY)
-            previewCtx.fillBrick(x, endBrickY - 1)
-        }
-        for (let y = startY; y !== endBrickY; y += Math.sign(endBrickY - startY)) {
-            previewCtx.fillBrick(startX, y)
-            previewCtx.fillBrick(endBrickX, y)
-        }
+            const dx = X - startX >= 0 ? 1 : -1
+            const dy = Y - startY >= 0 ? 1 : -1
 
+            for (let x = startX; x !== X + dx; x += dx) {
+                previewCtx.fillBrick(x, startY)
+                previewCtx.fillBrick(x, Y)
+            }
+            for (let y = startY; y !== Y; y += dy) {
+                previewCtx.fillBrick(startX, y)
+                previewCtx.fillBrick(X, y)
+            }
+
+        }, 'continuous': true
     },
-    function filledCircle(e) {
-        clearPreview()
-        drawEllipse(startX, startY, Math.abs(Math.floor(e.x / (brickSide * 1.2)) - startX), Math.abs(Math.floor((e.y - 48) / (brickSide * 1.2)) - startY), true)
+    {
+        'callback': function filledEllipse(X, Y) {
+            clearPreview()
+            drawEllipse(startX, startY, Math.abs(X - startX), Math.abs(Y - startY), true)
+        }, 'continuous': true
     },
-    function strokeCircle(e) {
-        clearPreview()
-        drawEllipse(startX, startY, Math.abs(Math.floor(e.x / (brickSide * 1.2)) - startX), Math.abs(Math.floor((e.y - 48) / (brickSide * 1.2)) - startY), false)
+    {
+        'callback': function strokeEllipse(X, Y) {
+            clearPreview()
+            drawEllipse(startX, startY, Math.abs(X - startX), Math.abs(Y - startY), false)
 
+        }, 'continuous': true
     },
-    function fill(e) {
+    {
+        'callback': function fill(X, Y) {
+            const targetColor = level[X][Y]
+            const buffer = [{ x: X, y: Y }]
+            let lastEl
 
+            while (buffer.length) {
+                lastEl = buffer.pop()
+                previewCtx.fillBrick(lastEl.x, lastEl.y)
+                X = lastEl.x
+                Y = lastEl.y
+
+                nextBricks = [{ x: X, y: Y - 1 }, { x: X + 1, y: Y }, { x: X, y: Y + 1 }, { x: X - 1, y: Y }]
+
+                for (let i = 0; i < 4; i++) {
+                    const brick = nextBricks[i];
+                    if (brick.x >= 0 && brick.x < 128 && brick.y >= 0 && brick.y <= 45 && 
+                        changeBuffer[brick.x][brick.y] === undefined && level[brick.x][brick.y] === targetColor) {
+                        buffer.push(brick)
+                        changeBuffer[brick.x][brick.y] = null
+                    }
+                }
+            }
+            endDraw()
+        }, 'continuous': false
     }
 ]
-
+let count = 0
 const modeButtons = document.querySelectorAll('.mode-btn')
 
 
@@ -160,33 +196,14 @@ function setMode(mode) {
     curMode = mode
 }
 
+function callbackWrapper(e) {
+    modes[curMode].callback(Math.floor(e.x / (brickSide * 1.2)), Math.floor((e.y - 48) / (brickSide * 1.2)))
+}
 
-document.addEventListener('keydown', (e) => {
-    if (e.keyCode === 90 && e.ctrlKey && undoBuffer.length > 1) {
-        for (const el of undoBuffer[undoBuffer.length - 2]) {
-            level[el.x][el.y] = el.color
-            ctx.fillStyle = el.color ?? '#212121'
-            ctx.fillRect(el.x * (brickSide * 1.2), el.y * (brickSide * 1.2), brickSide, brickSide);
-        }
-        undoBuffer.splice(undoBuffer.length - 2, 1)
-    }
-    if (e.key === 'e') { drawEllipse(0, 0, 128, 45, true) }
-})
-preview.addEventListener('mousedown', (e) => {
-    startX = Math.floor(e.x / (brickSide * 1.2))
-    startY = Math.floor((e.y - 48) / (brickSide * 1.2))
-    prewX = startX
-    prewY = startY
+//#region event listeners
 
-    ctx.fillStyle = picker.value
-    previewCtx.fillStyle = picker.value
-    previewCtx.fillRect(e.x - e.x % (brickSide * 1.2), e.y - e.y % (brickSide * 1.2) - 48, brickSide, brickSide)
-
-    preview.addEventListener('mousemove', modes[curMode])
-    modes[curMode](e)
-})
-document.addEventListener('mouseup', (e) => {
-    preview.removeEventListener('mousemove', modes[curMode])
+function endDraw() {
+    preview.removeEventListener('mousemove', callbackWrapper)
     previewCtx.clearRect(0, 0, canv.width, canv.height)
     for (let x = 0; x < 128; x++) {
         for (let y = 0; y < 45; y++) {
@@ -202,19 +219,38 @@ document.addEventListener('mouseup', (e) => {
     if (undoBuffer[undoBuffer.length - 1].length) {
         undoBuffer.push([])
     }
+}
+
+document.addEventListener('keydown', (e) => {
+    if (e.keyCode === 90 && e.ctrlKey && undoBuffer.length > 1) {
+        for (const el of undoBuffer[undoBuffer.length - 2]) {
+            level[el.x][el.y] = el.color
+            ctx.fillStyle = el.color ?? '#212121'
+            ctx.fillRect(el.x * (brickSide * 1.2), el.y * (brickSide * 1.2), brickSide, brickSide);
+        }
+        undoBuffer.splice(undoBuffer.length - 2, 1)
+    }
 })
+preview.addEventListener('mousedown', (e) => {
+    startX = Math.floor(e.x / (brickSide * 1.2))
+    startY = Math.floor((e.y - 48) / (brickSide * 1.2))
+    prewX = startX
+    prewY = startY
+
+    ctx.fillStyle = picker.value
+    previewCtx.fillStyle = picker.value
+
+    if (modes[curMode].continuous) { preview.addEventListener('mousemove', callbackWrapper) }
+    callbackWrapper(e)
+})
+document.addEventListener('mouseup', endDraw)
+//#endregion
 
 
-ctx.fillStyle = '#fff'
-previewCtx.fillStyle = '#fff'
-
-
-// Функция для сохранения уровня (сохраняем информацию о кирпичах)
 function saveLevel() {
 
 }
 
-// Функция для загрузки уровня (загружаем информацию о кирпичах)
 function loadLevel() {
 
 }
