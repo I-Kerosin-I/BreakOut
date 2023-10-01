037b9d1f9fa7ce038b5fd257b18188f0cdbcd2c7
I-Kerosin-I <max.andr2005@yandex.ru>
2023-10-01 19:45:33 +0300

    bonus system added

--- game/game.js
+++ game/game.js
@@ -1,29 +1,58 @@
 //#region canvases
 const canv = document.getElementById("mainCanv")
 const brickCanv = document.getElementById("bricksCanv")
+const bonusCanv = document.getElementById("bonusCanv")
 const ctx = canv.getContext('2d')
 const brickCtx = brickCanv.getContext('2d')
+const bonusCtx = bonusCanv.getContext('2d')
 
 canv.width = window.innerWidth;
 canv.height = window.innerHeight;
 brickCanv.width = window.innerWidth;
 brickCanv.height = window.innerHeight;
+bonusCanv.width = window.innerWidth;
+bonusCanv.height = window.innerHeight;
 
 //#endregion
 
 //#region constants
 
-const brickRowCount = 45;
-const brickColumnCount = 120;
+const bonusImgs = {
+    'add': new Image(),
+    'mul': new Image(),
+    'inc': new Image(),
+    'dec': new Image(),
+    'floor': new Image()
+}
 
-const brickWidth = 10;
-const brickHeight = 10;
+bonusImgs.add.src = 'img/add.png'
+bonusImgs.mul.src = 'img/mul.png'
+bonusImgs.inc.src = 'img/inc.png'
+bonusImgs.dec.src = 'img/dec.png'
+bonusImgs.floor.src = 'img/floor.png'
+
+
+const bonusSide = 24;
+const bonusSpeed = 100;
+const bonusChanceDec = 0.01;
+const bonusChanceInc = 0.002;
+let bonusChance = 0.1
+
+const brickRowCount = 30; // 45
+const brickColumnCount = 85;  // 128
+
+
+const brickWidth = Math.round(canv.width / (brickColumnCount - 1) / 1.2)
+const brickHeight = Math.round(canv.width / (brickColumnCount - 1) / 1.2)
+
 const brickPadding = (canv.width - brickWidth * brickColumnCount) / (brickColumnCount - 1);
 const ballRadius = 3;
-const ballSpeed = 3;
+const ballSpeed = 500;
+
 const paddleHeight = 10;
-const paddleWidth = 115;
+const startPaddleWidth = 115
 const paddleY = canv.height - paddleHeight * 2
+let paddleWidth = startPaddleWidth;
 //#endregion
 
 let fy = (bx, { x, y, velX, velY }) => y + (bx - x) * velY / velX
@@ -40,7 +69,6 @@ class Ball {
         this.cornerCollisions = []
     }
     calculateCollision() {
-        console.time('calculateCollision')
         this.verticalCollisions = []
         this.horizontalCollisions = []
         this.cornerCollisions = []
@@ -75,7 +103,6 @@ class Ball {
                 }
             }
         }
-        console.timeEnd('calculateCollision')
     }
 }
 
@@ -96,7 +123,69 @@ class Brick {
                 ball.splice(ball.findIndex((obj) => obj.id === this.id), 1)
             }
             bricks.splice(id, 1)
+            if (Math.random() < bonusChance / balls.length) { bonuses.push(new Bonus(this.x, this.y, ['add', 'mul', 'inc', 'dec', 'floor'][Math.floor(Math.random() * 5)])); bonusChance -= bonusChanceDec }
+            bonusChance += bonusChanceInc
         }
+    }
+}
+
+class Bonus {
+    constructor(x, y, type) {
+        this.x = x
+        this.y = y
+        this.type = type
+    }
+    check() {
+        if (this.y - bonusSide > canv.height) { return true }
+        if (this.y + bonusSide >= paddleY && this.y - bonusSide <= paddleY + paddleHeight && this.x + bonusSide >= paddleX && this.x - bonusSide <= paddleX + paddleWidth) {
+            switch (this.type) {
+                case 'add':
+                    const vel = Math.sqrt(ballSpeed ** 2 / 2)
+                    balls.push(new Ball(paddleX + paddleWidth / 2, paddleY - ballRadius, vel, -vel))
+                    balls.push(new Ball(paddleX + paddleWidth / 2, paddleY - ballRadius, 0.001, -ballSpeed,))
+                    balls.push(new Ball(paddleX + paddleWidth / 2, paddleY - ballRadius, -vel, -vel))
+                    for (let i = 1; i < 4; i++) { balls[balls.length - i].calculateCollision() }
+                    break;
+
+                case 'mul':
+                    console.time('mul')
+                    let ang
+                    const curLength = Math.min(balls.length, 100)
+                    for (let i = 0; i < curLength; i++) {
+                        ang = Math.acos(balls[i].velX / ballSpeed) * Math.sign(balls[i].velY / ballSpeed)
+                        balls.push(new Ball(balls[i].x, balls[i].y, ballSpeed * Math.cos(ang + Math.PI / 4), ballSpeed * Math.sin(ang + Math.PI / 4)))
+                        balls.push(new Ball(balls[i].x, balls[i].y, ballSpeed * Math.cos(ang - Math.PI / 4), ballSpeed * Math.sin(ang - Math.PI / 4)))
+
+                        for (let j = 1; j < 3; j++) { balls[balls.length - j].calculateCollision() }
+                    }
+                    console.timeEnd('mul')
+                    break;
+
+                case 'inc':
+                    paddleWidth += startPaddleWidth / 5
+                    break;
+
+                case 'dec':
+                    paddleWidth -= startPaddleWidth / 5
+                    break;
+
+                case 'floor':
+                    console.log('floor bonus catched!')
+                    break;
+
+                default:
+                    console.log('invalid bonus type!')
+                    break;
+            }
+            return true
+        }
+    }
+
+    draw() {
+        bonusCtx.drawImage(bonusImgs[this.type], this.x, this.y, bonusSide, bonusSide)
+    }
+    clear() {
+        bonusCtx.clearRect(this.x, this.y, bonusSide, bonusSide)
     }
 }
 //#region varaibles
@@ -104,6 +193,7 @@ let paddleX = (canv.width - paddleWidth) / 2;
 let oldPaddleX
 let balls = [new Ball(x = canv.width / 2, y = paddleY - ballRadius * 2, velX = 0, velY = 0)]
 let bricks = []
+const bonuses = []
 
 //#endregion
 
@@ -136,10 +226,10 @@ for (let c = 0; c < brickColumnCount; c++) {
 }
 
 //#region listenrs
-// window.addEventListener('resize', () => {
-//     canv.width = window.innerWidth;
-//     canv.height = window.innerHeight
-// }, false);
+window.addEventListener('resize', () => {
+    canv.width = window.innerWidth;
+    canv.height = window.innerHeight
+}, false);
 
 document.addEventListener('mousemove', (e) => {
     paddleX = e.x - paddleWidth / 2
@@ -149,12 +239,12 @@ function moveBall(e) {
     balls[0].x = Math.min(Math.max(e.x, ballRadius), canv.width - ballRadius)
 }
 function throwBall(e) {
-    balls[0].velY = -500
+    balls[0].velY = -ballSpeed
     balls[0].velX = 0.001
     balls[0].calculateCollision()
     document.removeEventListener('mousemove', moveBall)
     document.removeEventListener('mousedown', throwBall)
-    document.addEventListener('mousedown', (e) => { balls.push(new Ball(e.x, e.y, 0.1, -500)); balls[balls.length - 1].calculateCollision() })
+    document.addEventListener('mousedown', (e) => { balls.push(new Ball(e.x, e.y, 0.1, -ballSpeed)); balls[balls.length - 1].calculateCollision() })
 }
 document.addEventListener('mousemove', moveBall)
 document.addEventListener('mousedown', throwBall)
@@ -166,7 +256,6 @@ document.addEventListener('keydown', (e) => {
         draw()
     }
     if (e.key === 's') {
-        console.log(balls[0].x, balls[0].y, balls[0].velX, balls[0].velY, paddleX)
     }
 })
 
@@ -205,20 +294,20 @@ function clearBalls() {
 
 //#region paddle draw
 const paddleCanv = document.createElement('canvas');
-paddleCanv.width = paddleWidth * 2 + 1;
-paddleCanv.height = paddleHeight;
+paddleCanv.width = canv.width;
+paddleCanv.height = paddleHeight * 2;
 const paddleCtx = paddleCanv.getContext('2d');
 
 paddleCtx.fillStyle = "#787878";
-paddleCtx.fillRect(0, 0, paddleWidth, paddleHeight);
+paddleCtx.fillRect(0, 0, canv.width, paddleHeight);
 paddleCtx.fillStyle = "#212121";
-paddleCtx.fillRect(paddleWidth, 0, paddleWidth + 1, paddleHeight);
+paddleCtx.fillRect(0, paddleHeight, canv.width, paddleHeight);
 
 function drawPaddle() {
     ctx.drawImage(paddleCanv, 0, 0, paddleWidth, paddleHeight, paddleX, paddleY, paddleWidth, paddleHeight)
 }
 function clearPaddle() {
-    ctx.drawImage(paddleCanv, paddleWidth + 1, 0, paddleWidth, paddleHeight, oldPaddleX, paddleY, paddleWidth + 1, paddleHeight)
+    ctx.drawImage(paddleCanv, 0, paddleHeight, paddleWidth, paddleHeight, oldPaddleX, paddleY, paddleWidth + 1, paddleHeight)
 }
 //#endregion
 
@@ -301,10 +390,9 @@ function collisionDetection(dTime) {
                 ball.x = fx(paddleY, ball)
                 ball.y = paddleY
                 const ang = (1 - (ball.x - paddleX) / paddleWidth) * 3.14
-                const vel = Math.sqrt(ball.velX ** 2 + ball.velY ** 2)
 
-                ball.velX = vel * Math.cos(ang)
-                ball.velY = -vel * Math.sin(ang)
+                ball.velX = ballSpeed * Math.cos(ang)
+                ball.velY = -ballSpeed * Math.sin(ang)
 
                 ball.calculateCollision()
                 continue ballLoop
@@ -314,7 +402,6 @@ function collisionDetection(dTime) {
             balls.splice(i, 1)
             if (!balls.length) {
                 location.reload()
-                console.log('game over')
             }
             continue ballLoop
         }
@@ -329,12 +416,19 @@ function draw() {
     let dTime = (curTime - startTime) / 1000
     startTime = curTime
 
+    for (let i = 0; i < bonuses.length; i++) {
+        bonuses[i].clear()
+        bonuses[i].y += bonusSpeed * dTime
+        if (bonuses[i].check()) { bonuses.splice(i, 1) }
+    }
+    for (let i = 0; i < bonuses.length; i++) { bonuses[i].draw() }
     clearBalls()
     collisionDetection(dTime);
     drawBalls();
     clearPaddle()
     drawPaddle();
     oldPaddleX = paddleX
+
 }
 drawBricks();
 let startTime = new Date().getTime()
