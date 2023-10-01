9008313c4c1daefb34e237a694f042538842b291
I-Kerosin-I <max.andr2005@yandex.ru>
2023-10-01 19:43:05 +0300

    symmetry added palette added

--- editor/script.js
+++ editor/script.js
@@ -5,42 +5,60 @@ const preview = document.getElementById('preview')
 const ctx = canv.getContext('2d')
 const previewCtx = preview.getContext('2d')
 
-
-const brickImg = ctx.createImageData(10, 10);
-
-canv.width = window.innerWidth;
 canv.height = window.innerHeight - 48;
+canv.width = canv.height * 2.078484438430311;
 
-preview.width = window.innerWidth;
 preview.height = window.innerHeight - 48;
+preview.width = preview.height * 2.078484438430311;
 
-const brickSide = 10
+const brickRowCount = 30; // 45
+const brickColumnCount = 85;  // 128
+const brickSide = Math.round(canv.width / (brickColumnCount - 1) / 1.2)
 
-
-const picker = document.getElementById('colorPicker')
+const colorCircles = document.querySelectorAll('.color-circle');
+const colorPicker = document.getElementById('color-picker')
 
 
 const level = []
 let undoBuffer = [[]]
 let changeBuffer = []
+let XSymmetry = false
+let YSymmetry = false
 
-for (let x = 0; x < 128; x++) {
+for (let x = 0; x < brickColumnCount; x++) {
     level.push([])
     changeBuffer.push([])
 }
 
 let curMode = 0
+let curColorCircle = colorCircles[1]
+let breakable = true
 let startX, startY
 let prewX, prewY
 
 CanvasRenderingContext2D.prototype.fillBrick = function (x, y) {
+    if (x < 0 || x > (brickColumnCount - 1) || y < 0 || y > (brickRowCount - 1)) { return }
+
     this.fillRect(x * (brickSide * 1.2), y * (brickSide * 1.2), brickSide, brickSide);
-    changeBuffer[x] ? changeBuffer[x][y] = previewCtx.fillStyle : undefined
+    changeBuffer[x][y] = [previewCtx.fillStyle, breakable]
+
+    if (YSymmetry) {
+        this.fillRect((brickColumnCount - 1 - x) * (brickSide * 1.2), y * (brickSide * 1.2), brickSide, brickSide);
+        changeBuffer[brickColumnCount - 1 - x][y] = [previewCtx.fillStyle, breakable]
+    }
+    if (XSymmetry) {
+        this.fillRect(x * (brickSide * 1.2), (brickRowCount - 1 - y) * (brickSide * 1.2), brickSide, brickSide);
+        changeBuffer[x][brickRowCount - 1 - y] = [previewCtx.fillStyle, breakable]
+    }
+    if (XSymmetry && YSymmetry) {
+        this.fillRect((brickColumnCount - 1 - x) * (brickSide * 1.2), (brickRowCount - 1 - y) * (brickSide * 1.2), brickSide, brickSide);
+        changeBuffer[brickColumnCount - 1 - x][brickRowCount - 1 - y] = [previewCtx.fillStyle, breakable]
+    }
 }
 
 function clearPreview() {
     previewCtx.clearRect(0, 0, canv.width, canv.height)
-    for (let x = 0; x < 128; x++) {
+    for (let x = 0; x < brickColumnCount; x++) {
         changeBuffer[x] = []
     }
 }
@@ -60,7 +78,6 @@ function drawLine(x0, y0, x1, y1) {
     }
 }
 
-
 function drawEllipse(X1, Y1, a, b, filled) {
     let y = b
     let x = 0
@@ -68,7 +85,7 @@ function drawEllipse(X1, Y1, a, b, filled) {
         drawLine(X1 - Math.floor(a * 0.8), Y1, X1 + Math.floor(a * 0.8), Y1)
         return
     }
-    if (Y1 + b < 45) { previewCtx.fillBrick(X1, Y1 + b) }
+    if (Y1 + b < brickRowCount) { previewCtx.fillBrick(X1, Y1 + b) }
     if (Y1 - b >= 0) { previewCtx.fillBrick(X1, Y1 - b) }
 
     while (y > 0) {
@@ -83,15 +100,15 @@ function drawEllipse(X1, Y1, a, b, filled) {
             }
             y--
         }
-        if (X1 + x < 128 && Y1 - y >= 0) { previewCtx.fillBrick(X1 + x, Y1 - y) }
+        if (X1 + x < brickColumnCount && Y1 - y >= 0) { previewCtx.fillBrick(X1 + x, Y1 - y) }
         if (X1 - x > 0 && Y1 - y >= 0) { previewCtx.fillBrick(X1 - x, Y1 - y) }
-        if (X1 + x < 128 && Y1 + y < 45) { previewCtx.fillBrick(X1 + x, Y1 + y) }
-        if (X1 - x > 0 && Y1 + y < 45) { previewCtx.fillBrick(X1 - x, Y1 + y) }
+        if (X1 + x < brickColumnCount && Y1 + y < brickRowCount) { previewCtx.fillBrick(X1 + x, Y1 + y) }
+        if (X1 - x > 0 && Y1 + y < brickRowCount) { previewCtx.fillBrick(X1 - x, Y1 + y) }
         if (filled) {
-            const lastX = Math.min(X1 + x, 128)
+            const lastX = Math.min(X1 + x, brickColumnCount)
             for (let i = Math.max(X1 - x, 0); i < lastX; i++) {
                 if (Y1 - y >= 0) { previewCtx.fillBrick(i, Y1 - y) }
-                if (Y1 + y < 45) { previewCtx.fillBrick(i, Y1 + y) }
+                if (Y1 + y < brickRowCount) { previewCtx.fillBrick(i, Y1 + y) }
             }
         }
     }
@@ -175,7 +192,7 @@ const modes = [
 
                 for (let i = 0; i < 4; i++) {
                     const brick = nextBricks[i];
-                    if (brick.x >= 0 && brick.x < 128 && brick.y >= 0 && brick.y <= 45 && 
+                    if (brick.x >= 0 && brick.x < brickColumnCount && brick.y >= 0 && brick.y <= brickRowCount &&
                         changeBuffer[brick.x][brick.y] === undefined && level[brick.x][brick.y] === targetColor) {
                         buffer.push(brick)
                         changeBuffer[brick.x][brick.y] = null
@@ -186,15 +203,15 @@ const modes = [
         }, 'continuous': false
     }
 ]
-let count = 0
 const modeButtons = document.querySelectorAll('.mode-btn')
 
 
 function setMode(mode) {
-    modeButtons[curMode].style.removeProperty("background-color")
+    modeButtons[curMode].style.removeProperty('background-color')
     modeButtons[mode].style.backgroundColor = '#4CAF50'
     curMode = mode
 }
+
 
 function callbackWrapper(e) {
     modes[curMode].callback(Math.floor(e.x / (brickSide * 1.2)), Math.floor((e.y - 48) / (brickSide * 1.2)))
@@ -205,13 +222,13 @@ function callbackWrapper(e) {
 function endDraw() {
     preview.removeEventListener('mousemove', callbackWrapper)
     previewCtx.clearRect(0, 0, canv.width, canv.height)
-    for (let x = 0; x < 128; x++) {
-        for (let y = 0; y < 45; y++) {
+    for (let x = 0; x < brickColumnCount; x++) {
+        for (let y = 0; y < brickRowCount; y++) {
             const el = changeBuffer[x][y]
             if (el) {
-                undoBuffer[undoBuffer.length - 1].push({ x: x, y: y, color: level[x][y] })
+                undoBuffer[undoBuffer.length - 1].push({ x: x, y: y, brick: level[x][y] })
                 level[x][y] = el
-                ctx.fillBrick(x, y)
+                ctx.fillRect(x * (brickSide * 1.2), y * (brickSide * 1.2), brickSide, brickSide)
                 changeBuffer[x][y] = undefined
             }
         }
@@ -220,25 +237,49 @@ function endDraw() {
         undoBuffer.push([])
     }
 }
+let target
+
+colorPicker.addEventListener('input', (e) => { target.style.backgroundColor = colorPicker.value });
+colorCircles.forEach((circle, index) => {
+    circle.addEventListener('click', () => {
+        colorCircles.forEach((circle, index) => {
+            circle.classList.toggle('selected', false);
+        })
+        circle.classList.toggle('selected', true)
+        curColorCircle = circle
+    });
+
+    if (index > 0 && index < colorCircles.length - 1) {
+        circle.addEventListener('contextmenu', (e) => {
+            target = e.target
+            e.preventDefault();
+            colorPicker.click();
+        })
+    }
+})
+
+
 
 document.addEventListener('keydown', (e) => {
     if (e.keyCode === 90 && e.ctrlKey && undoBuffer.length > 1) {
         for (const el of undoBuffer[undoBuffer.length - 2]) {
-            level[el.x][el.y] = el.color
-            ctx.fillStyle = el.color ?? '#212121'
+            level[el.x][el.y] = el.brick
+            ctx.fillStyle = el.brick?.[0] ?? '#212121'
             ctx.fillRect(el.x * (brickSide * 1.2), el.y * (brickSide * 1.2), brickSide, brickSide);
         }
         undoBuffer.splice(undoBuffer.length - 2, 1)
     }
 })
 preview.addEventListener('mousedown', (e) => {
+    if (e.button !== 0) { return }
+
     startX = Math.floor(e.x / (brickSide * 1.2))
     startY = Math.floor((e.y - 48) / (brickSide * 1.2))
     prewX = startX
     prewY = startY
 
-    ctx.fillStyle = picker.value
-    previewCtx.fillStyle = picker.value
+    ctx.fillStyle = curColorCircle.style.backgroundColor
+    previewCtx.fillStyle = curColorCircle.style.backgroundColor
 
     if (modes[curMode].continuous) { preview.addEventListener('mousemove', callbackWrapper) }
     callbackWrapper(e)
